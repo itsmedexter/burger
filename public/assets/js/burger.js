d93f9f1d979eff0a4f01b4b51f1df936be9f0c96
Dexter Valencia <deeside@pacbell.net>
2020-01-22 23:02:05 -0800

    added some css styling

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,8 +1,27 @@
 $(function() {
+    $(".change-sleep").on("click", function(event){
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+
+        var newDevourState = {
+            devoured: newDevour
+        };
+        $.ajax("api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("changed devour to", newDevour);
+                location.reload();
+            }
+        );
+    });
+
+
     $(".create-form").on("submit", function(event) {
+        event.preventDefault();
         var newBurger = {
             burger_name: $("#newburger").val().trim(),
-            devoured: 0
         };
 
         $.ajax("/api/burgers", {
